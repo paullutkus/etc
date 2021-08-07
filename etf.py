e6b5dc8550b7d8d3c3233764728b5852a5fdbe06
Mit Ramesh <mithun.ramesh19@trinityschoolnyc.org>
2021-08-07 15:24:14 -0400

    Fixed ETF

--- etf.py
+++ etf.py
@@ -9,18 +9,18 @@ price
 """
 
 def etf_trade_mean(fair_value_book, key):
-    return sum(fair_value_book["key"])/len(fair_value_book["key"])
+    return fair_value_book["key"][0]
 
 def calculate_ETF_order_type(fair_value_book, positions):
-    if len((fair_value_book["XLF"])) < 25:
+    if fair_value_book["XLF"][1] < 25:
         return None
-    conversion_cost = 150
-    etf_trade_value = etf_trade_mean(fair_value_book, "XLF")
+    conversion_cost = 100
+    ETF_MEAN = etf_trade_mean(fair_value_book, "XLF")
     WFC_MEAN = etf_trade_mean(fair_value_book, "WFC")
     GS_MEAN = etf_trade_mean(fair_value_book, "GS")
     MS_MEAN = etf_trade_mean(fair_value_book, "MS")
     BOND_FAIR = 300
-    if 10 * etf_trade_value + conversion_cost < BOND_FAIR + 2 * WFC_MEAN + 3 * MS_MEAN + 2*GS_MEAN:
+    if 10 * ETF_MEAN + conversion_cost < BOND_FAIR + 2 * WFC_MEAN + 3 * MS_MEAN + 2*GS_MEAN:
         can_trade = can_trade_be_done(positions, "BUY_XLF")
         if can_trade:
             actions = [
@@ -29,11 +29,11 @@ def calculate_ETF_order_type(fair_value_book, positions):
                 {"type": "add", "order_id": order_id, "symbol": "MS", "dir": "SELL", "price": MS_MEAN + 1, "size": 30},
                 {"type": "add", "order_id": order_id, "symbol": "WFC", "dir": "SELL", "price": WFC_MEAN + 1, "size": 20},
                 {"type": "add", "order_id": order_id, "symbol": "GS", "dir": "SELL", "price": GS_MEAN + 1, "size": 20},
-                {"type": "add", "order_id": order_id, "symbol": "XLF", "dir": "BUY", "price": etf_trade_value - 1, "size": 100},
+                {"type": "add", "order_id": order_id, "symbol": "XLF", "dir": "BUY", "price": ETF_MEAN - 1, "size": 100},
             ]
         else:
             return None
-    elif  10 * etf_trade_value - conversion_cost > BOND_FAIR + 2 * WFC_MEAN + 3 * MS_MEAN + 2*GS_MEAN:
+    elif 10 * ETF_MEAN + conversion_cost > BOND_FAIR + 2 * WFC_MEAN + 3 * MS_MEAN + 2*GS_MEAN:
         can_trade = can_trade_be_done(positions, "SELL_XLF")
         if can_trade:
             actions = [
@@ -42,7 +42,7 @@ def calculate_ETF_order_type(fair_value_book, positions):
                 {"type": "add", "order_id": order_id, "symbol": "MS", "dir": "BUY", "price": MS_MEAN - 1, "size": 30},
                 {"type": "add", "order_id": order_id, "symbol": "WFC", "dir": "BUY", "price": WFC_MEAN - 1, "size": 20},
                 {"type": "add", "order_id": order_id, "symbol": "GS", "dir": "BUY", "price": GS_MEAN - 1, "size": 20},
-                {"type": "add", "order_id": order_id, "symbol": "XLF", "dir": "SELL", "price": etf_trade_value + 1, "size": 100}
+                {"type": "add", "order_id": order_id, "symbol": "XLF", "dir": "SELL", "price": ETF_MEAN + 1, "size": 100}
             ]
         else:
             return None
