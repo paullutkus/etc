40891d02604f18c83e6f0441f38ecf8387d132f2
Ubuntu <ubuntu@bot-rector.2021-second.js-etc>
2021-08-07 18:20:22 +0000

    push all

--- bond_json.py
+++ bond_json.py
@@ -6,18 +6,18 @@ import json
 Method evaluates bond order based on price. Returns a json
 order to execute
 """
-def evaluate_bond_order(book, order_id):
+def evaluate_bond_order(book, order_id, positions):
     if book['BOND'][0] == 0:
-        return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 100}
+        return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 1}
     if book['BOND'][1] == 0:
-        return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": 1001, "size": 100}
+        return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": 1001, "size": 1}
     buy_price = book['BOND'][0][0]
     sell_price = book['BOND'][1][0]
     book_buy_size, book_sale_size = calculate_positions(positions)
     if sell_price > 1000:
-       return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "SELL", "price": sell_price, "size": book_buy_size}
+       return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "SELL", "price": sell_price, "size": 1}
     if buy_price < 1000:
-        return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": buy_price, "size": book_sale_size}
+        return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": buy_price, "size": 1}
     else:
         return None
 
@@ -30,7 +30,7 @@ def balance_fill(filL_order):
        else:
             return {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": 999, "size": message["size"]}
 
-def calculate_positions(positions, trade):
+def calculate_positions(positions):
     if positions["BOND"] == 0:
         return 100, 100
     else:
