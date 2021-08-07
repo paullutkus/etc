f0d1cb5fef911314654dcf785a912b59cb8d2efe
Ubuntu <ubuntu@bot-rector.2021-second.js-etc>
2021-08-07 15:08:24 +0000

    fixed string formatting for bond.py

--- bond.py
+++ bond.py
@@ -2,8 +2,8 @@
 
 def trade_bond(book):
     if book['BOND'][0] > 1000:
-       return f'ADD 1 BOND SELL {book['BOND'][0] 1}'
+       return "ADD 1 BOND SELL %d 1"(book['BOND'][0])
     elif book['BOND'][1] < 1000:
-       return f'ADD 1 BOND BUY {book['BOND'][1] 1}'
+       return "ADD 1 BOND BUY %d 1"(book['BOND'][1]) 
 
 
